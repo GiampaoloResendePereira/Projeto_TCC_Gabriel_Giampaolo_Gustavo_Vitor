218085ed6a07a328d85c676fb19fb7325c022a89
GustavoSantanaprogramador <gustavodiassantanads@gmail.com>
2024-10-10 16:34:11 -0300

    Subindo atualização com o json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import CalculoFrete from './components/pages/CalculoFrete';
+import EditEntregas from './components/pages/EditEntregas';
 import GerenciamentoEntregas from './components/pages/GerenciamentoEntregas';
 import Home from './components/pages/Home'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -13,6 +14,7 @@ function App() {
         <Route path ='/' element={<Home/>}/>
         <Route path ='/gerenciamento_entregas' element={<GerenciamentoEntregas/>}/>
         <Route path ='/calculo_frete' element={<CalculoFrete/>}/>
+        <Route path ='/editar_entregas' element={<EditEntregas/>}/>
       </Routes>
     </Router>
   );
--- client/src/components/pages/GerenciamentoEntregas.jsx
+++ client/src/components/pages/GerenciamentoEntregas.jsx
@@ -6,41 +6,46 @@ function Gerenciamento_entregas() {
   return (
 
     <>
-    <NavBarAdmin/>
+      <NavBarAdmin />
 
-    <div className='LayoutEntregas'>
-      <div className='form-select'>
-        <div>Status
-          <form action="">
-            <input type="radio" id='option1' name='group1' value='Option1' />
-            <label for='option1'>Todos</label>
-            <input type="radio" id='option2' name='group1' value='Option1' />
-            <label for='option2'>Agendados</label>
-            <input type='radio' id='option3' name='group1' value='Option1' />
-            <label for='option3'>Em andamento</label>
-            <input type='radio' id='option4' name='group1' value='Option1' />
-            <label for='option4'>Finalizados</label>
-          </form>
-          {/* <div className='date'> */}
+      <div className='LayoutEntregas'>
+        <div className='form-select'>
+          <div>Filtro de pesquisa
+            <form action="">
+              <div>Status
+                <input type="radio" id='option1' name='group1' value='Option1' />
+                <label for='option1'>Todos</label>
+                <input type="radio" id='option2' name='group1' value='Option1' />
+                <label for='option2'>Agendados</label>
+                <input type='radio' id='option3' name='group1' value='Option1' />
+                <label for='option3'>Em andamento</label>
+                <input type='radio' id='option4' name='group1' value='Option1' />
+                <label for='option4'>Finalizados</label>
+              </div>
+            </form>
+            {/* <div className='date'> */}
+          </div>
         </div>
-      </div>
-      <div>
-        <div className= 'container'>
-          <div className='blackTable' >
-            <table className='tituloPesquisa'>
-              <thead>
-                <span>id</span>
-                <span>Cliente</span>
-                <span>Motoboy</span>
-                <span>Status</span>
-                <span>Data Agendada</span>
-                <span>Data Entrega</span>
-              </thead>
-            </table>
+        <div>
+          <div className='container'>
+            <div className='blackTable' >
+              <table className='tituloPesquisa'>
+                <thead>
+                  <span>id</span>
+                  <span>Cliente</span>
+                  <span>Motoboy</span>
+                  <span>Status</span>
+                  <span>Data Agendada</span>
+                  <span>Data Entrega</span>
+                  <span class ="bg-light">
+                    <a class="btn btn-warning" href='/editar_entregas'>Editar</a>
+                  </span>
+                </thead>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
