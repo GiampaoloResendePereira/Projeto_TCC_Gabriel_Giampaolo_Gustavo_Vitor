4caceb6acda80daa81ea5a5e58950d8a9add7901
GiampaoloResendePereira <giampaolopereira29@gmail.com>
2024-10-11 20:45:00 -0300

    Administrador

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TelaAdministrador from './pages/Administrador/TelaAdministrador';
+import GerenciamentoEntrega from './pages/GerenciamentoEntrega';
+import EditarParametro from './pages/EditarParametro';
+import EditarPerfis from './pages/EditarPerfis';
+import Relatorios from './pages/Relatorios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/pages/Home';
-import Login from './components/pages/Login';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path="/" element={<TelaAdministrador />} />
+        <Route path="/gerenciamento-entregas" element={<GerenciamentoEntrega />} />
+        <Route path="/editar-parametro" element={<EditarParametro />} />
+        <Route path="/editar-perfis" element={<EditarPerfis />} />
+        <Route path="/relatorios" element={<Relatorios />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
