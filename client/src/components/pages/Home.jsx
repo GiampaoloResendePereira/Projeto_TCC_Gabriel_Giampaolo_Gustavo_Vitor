4779c537bcb3e2bde89d44baeb8ae69532905f2f
GUSTAVO DIAS SANTANA <gsantana@labcetec.net>
2024-11-05 21:20:46 -0300

    ajustando merge e resolvendo o conflito

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import CalculoFrete from './CalculoFrete'
 import EntregasGet from '../Crude/EntregasGet'
-import Gerenciamento_entregas from './GerenciamentoEntregas'
+import Gerenciamento_entregas from './Gerenciamento'
 import Login from '../Inicio/Login'
 
 function Home() {
